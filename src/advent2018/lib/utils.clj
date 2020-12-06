fd5cbcd3769b13e0718be2ed01783f6385976351
Ken Smith <ken.smith@2scientists.net>
2020-12-06 10:38:26 -0700

    Add solns and tests for 2018 day04

--- src/advent2018/lib/utils.clj
+++ src/advent2018/lib/utils.clj
@@ -7,3 +7,14 @@
        io/resource
        io/reader
        line-seq))
+
+(defn fmap
+  "Applies the function f to the values of the map m"
+  [f m]
+  (zipmap (keys m) (map f (vals m))))
+
+(defn max-val
+  "Returns the key in a map m for which the value is the greatest.
+   If there are multiple such keys, the first is returned"
+  [m]
+  (key (apply max-key val m)))
